6a3a202387fd85d34e41d1506b28708c1d68c4a3
KhaledElfakharany <K.fakharany@gmail.com>
2017-06-04 22:56:21 +0200

    Initial Commit

---
+++ IR(Ipad version)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IR(Ipad version)
+//
+//  Created by Khaled Elfakharany on 6/4/17.
+//  Copyright © 2017 Khaled Elfakharany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
